ce048adb01fca158205d5c3fcb30266b6f769206
Hyoin-Kim <hi980506@naver.com>
2021-06-10 17:54:42 +0900

    diary-app

--- diary-app/src/pages/Diary.js
+++ diary-app/src/pages/Diary.js
@@ -1,15 +1,23 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { withRouter } from "react-router-dom";
 import { getCardData } from "../lib/api";
 import Card from "../components/diary/Card";
 
 const Diary = ({ year, month, match }) => {
-  //const id = match.params.id;
-  console.log(match);
-  //const [diaryData, setDiaryData] = useState(null);
-  //const [rawData, setRawData] = useState(null);
+  const id = match.params.id;
+  const [diaryData,setDiaryData] = React.useState(null);
 
-  return <div>diary page</div>
+  React.useEffect(() => {
+    (async () => {
+      const data = await getCardData();
+      data[year] &&
+        setDiaryData(data[year][month].find((el) => el.id === parseInt(id)));
+        console.log(diaryData);
+    })();
+  }, [id]);
+
+
+  return diaryData && <Card data={diaryData} />
 
 
 
